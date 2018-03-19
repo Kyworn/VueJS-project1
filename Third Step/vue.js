77053d980a38f59e7025409c84c6bfe9608cee38
Idriss Messaoud <idrissmessaoud@macbook-pro-de-idriss.home>
2018-03-20 00:18:16 +0100

    Changement de VueJs dev mod en prod mod et nom de l'instance

--- Third Step/vue.js
+++ Third Step/vue.js
@@ -1,11 +1,11 @@
 function addTodo() {
   var valueList = document.getElementById('todo-add').value;
-  app7.todos.push({text: valueList});
+  todoList.todos.push({text: valueList});
   document.getElementById('todo-add').value = "";
 }
 
-var app7 = new Vue({
-  el: '#app-7',
+var todoList = new Vue({
+  el: '#todo-list',
   data: {
     todos: [
 
