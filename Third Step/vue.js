3d00eb2ce147fc2e686be108a202f8b8f0179c87
Idriss Messaoud <idrissmessaoud@macbook-pro-de-idriss.home>
2018-03-20 00:37:23 +0100

    Stylisation

--- Third Step/vue.js
+++ Third Step/vue.js
@@ -1,7 +1,17 @@
 function addTodo() {
   var valueList = document.getElementById('todo-add').value;
-  todoList.todos.push({text: valueList});
-  document.getElementById('todo-add').value = "";
+  if (valueList !== ""){
+    todoList.todos.push({text: valueList});
+    document.getElementById('todo-add').value = "";
+  }
+}
+
+function deleteTodo() {
+  var valueList = document.getElementById('todo-delete').value;
+  if (valueList !== ""){
+    Vue.delete(todoList.todos, valueList - 1);
+    document.getElementById('todo-delete').value = "";
+  }
 }
 
 var todoList = new Vue({
