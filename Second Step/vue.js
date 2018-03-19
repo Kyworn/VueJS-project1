4e82299deac1e5ba550603966e638be3accbfdf7
Idriss Messaoud <idrissmessaoud@macbook-pro-de-idriss.home>
2018-03-19 17:54:23 +0100

    Prise en main deuxième etape

---
+++ Second Step/vue.js
@@ -0,0 +1,19 @@
+// Définit un nouveau composant appelé todo-item
+Vue.component('todo-item', {
+  // Le composant todo-item accepte maintenant une
+  // « prop » qui est comme un attribut personnalisé.
+  // Cette prop est appelée todo.  props: ['todo'],
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Légumes' },
+      { id: 1, text: 'Fromage' },
+      { id: 2, text: 'Tout ce que les humains sont supposés manger' }
+    ]
+  }
+})
